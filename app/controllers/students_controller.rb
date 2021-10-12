0dcffffe0f1606c67bd05f5afde58ced69420586
Theo Ferguson <ferguson.theo@gmail.com>
2021-10-12 13:01:08 -0400

    commit tree a0c4bbd06562f0581759d54c39001dbddfa329ad

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    students = if params[:name]
+                  Student.by_name(params[:name])
+                else
+                  Student.all
+                end
+                render json: students
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+    
   end
 
 end
